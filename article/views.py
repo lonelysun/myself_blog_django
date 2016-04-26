f7743cdcbefde2f6bb46e7ec4a9c4758d055c60e
lonely_sun <juebanliuhao@163.com>
2016-04-26 19:19:38 +0800

    commit

--- article/views.py
+++ article/views.py
@@ -1,10 +1,13 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
 from article.models import Article
+from datetime import datetime
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello World, Django")
+    post_list = Article.objects.all()  #获取全部的Article对象
+    return render(request, 'home.html', {'post_list' : post_list})
 
 def detail(request, my_args):
     post = Article.objects.all()[int(my_args)]
